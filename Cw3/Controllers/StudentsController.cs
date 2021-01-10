5e633061858dab54e91e05ae1ce12c2da16064f6
RobertRaczkowski <RobertRaczkowski@DESKTOP-EIH1V3C>
2021-01-10 14:40:21 +0100

    zadanie 3

--- Cw3/Controllers/StudentsController.cs
+++ Cw3/Controllers/StudentsController.cs
@@ -16,5 +16,19 @@ namespace Cw3.Controllers
         {
             return "Jan, Anna, Krzysztof";
         }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+
+            return NotFound("Nie znaleziono studenta");
+        }
     }
 }
