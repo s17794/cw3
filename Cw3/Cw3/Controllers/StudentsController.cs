5e9802377b0a4d93899daca2a8e1eb980730b4bc
RobertRaczkowski <RobertRaczkowski@DESKTOP-EIH1V3C>
2021-01-10 14:23:19 +0100

    zadanie 3

---
+++ Cw3/Cw3/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw3.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudents()
+        {
+            return "Jan, Anna, Krzysztof";
+        }
+    }
+}
